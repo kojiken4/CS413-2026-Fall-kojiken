eceb02d3e186df6b49bf3fc54e3fc187f51cebd7
Koji Aquilino <kojiken5678@gmail.com>
2026-09-07 19:55:33 -0400

    Add remaining tests for queen interactions

--- assigns/01/MySolution/test_queens.py
+++ assigns/01/MySolution/test_queens.py
@@ -1,5 +1,5 @@
 import unittest
-from queens import safety_test1
+from queens import safety_test1, board_get
 
 
 
@@ -9,6 +9,16 @@ class TestQueens(unittest.TestCase):
     def test_safety_test1_reject_same_diagonal(self):
         self.assertFalse(safety_test1(i0=1, j0=1, i=0, j=0))
         self.assertFalse(safety_test1(i0=7, j0=5, i=4, j=2))
+
+    # testing safety test 1 with queens on same row, should be false
+    def test_safety_test1_reject_same_row(self):
+        self.assertFalse(safety_test1(i0=1, j0=1, i=1, j=0))
+        self.assertFalse(safety_test1(i0=7, j0=6, i=7, j=1))
+
+    # testing safety test 1 with queens on same column, should be false
+    def test_safety_test1_reject_same_column(self):
+        self.assertFalse(safety_test1(i0=1, j0=1, i=0, j=1))
+        self.assertFalse(safety_test1(i0=7, j0=5, i=1, j=5))
 
 
 
