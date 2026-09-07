1056a697c51cb9dd869fd56634e71a6953607e01
Koji Aquilino <kojiken5678@gmail.com>
2026-09-07 19:04:13 -0400

    Add unit test file with elementary test for validating queens functionality

---
+++ assigns/01/MySolution/test_queens.py
@@ -0,0 +1,16 @@
+import unittest
+from queens import safety_test1
+
+
+
+class TestQueens(unittest.TestCase):
+
+    # testing safety test 1 with queens on same diagonal, should be false
+    def test_safety_test1_reject_same_diagonal(self):
+        self.assertFalse(safety_test1(i0=1, j0=1, i=0, j=0))
+        self.assertFalse(safety_test1(i0=7, j0=5, i=4, j=2))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
